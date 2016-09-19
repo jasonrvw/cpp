14bc3f8ea56743943139dd58539733c602641002
Jason van Wyngaard <jasonv@hyperionconsulting.co.za>
2016-09-19 19:43:27 +0200

    Lecture02_Section19

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+
+// the entry point of our application
+int main()
+{
+	PrintIntro();
+	PlayGame();
+	
+	cout << endl;
+	return 0;
+}
+
+// introduce the game
+void PrintIntro() 
+{
+	constexpr int WORD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?" << endl;
+	return;
+}
+
+string GetGuess()
+{
+	// get a guess from the player
+	string Guess = "";
+	cout << endl;
+	cout << "My guess is: ";
+	getline(cin, Guess);
+	return Guess;
+}
+
+// loop for the number of turns asking for guesses
+void PlayGame()
+{
+	constexpr int NUM_GUESS = 5;
+	for (int count = 1; count <= NUM_GUESS; count++)
+	{
+		string Guess = "";
+		GetGuess() = Guess;
+		cout << GetGuess();
+	}
+	cout << "Your guess was: " << GetGuess << endl;
+	return;
+}
+
+
