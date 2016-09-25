fcaae348f0430d9f90b8ade5266c3636874d39f5
Jason van Wyngaard <jasonv@hyperionconsulting.co.za>
2016-09-25 13:13:24 +0200

    Lecture_21_do_while

--- main.cpp
+++ main.cpp
@@ -13,9 +13,11 @@ string GetGuess();
 // the entry point of our application
 int main()
 {
-	PrintIntro();
-	PlayGame();
-	AskToPlayAgain();
+	do
+	{
+		PrintIntro();
+		PlayGame();
+	} while (AskToPlayAgain());	
 	
 	cout << endl;
 	return 0;
@@ -57,7 +59,7 @@ string GetGuess()
 
 bool AskToPlayAgain()
 {
-	cout << "Do you want to play again? ";
+	cout << "Do you want to play again? (y/n) ";
 	string Response = "";
 	getline(cin, Response);
 	return (Response[0] == 'y') || (Response[0] == 'Y');
