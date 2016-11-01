91678d1a2d86565f73e4d942917092be0133add8
Jason van Wyngaard <jasonv@hyperionconsulting.co.za>
2016-11-01 16:16:55 +0200

    Lecture44_Containers and Iterators

--- FBullCowGame.cpp
+++ FBullCowGame.cpp
@@ -107,8 +107,22 @@ EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const
 	
 }
 
-bool FBullCowGame::IsIsogram(FString) const
+bool FBullCowGame::IsIsogram(FString Word) const
 {
-	return true;
+	// treat 0 and 1 letter words as isograms
+	if (Word.length() <= 1) { return true; }
+
+	TMap <char, bool> LetterSeen; // setup our map
+	for (auto Letter : Word) // for all the characters in the word
+	{
+		Letter = tolower(Letter); // handle mixed case
+		if (LetterSeen[Letter]) { // if the letter is in the map
+			return false; // we do NOT have an isogram
+		}
+		else {
+			LetterSeen [Letter] = true; // add the letter to the map as seen
+		}
+	}
+	return true; // for example in cases where /0 is entered
 }
 
